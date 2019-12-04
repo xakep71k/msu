832063b26d36d9885afb8173893eda401078dfcf
pan-sultan <alexander.bubnov@gmail.com>
2019-12-04 09:15:12 +0300

    project 17 added

---
+++ project_17_2019/project_17.pas
@@ -0,0 +1,13 @@
+{
+Методы сортировки (зачетный проект)
+}
+type CatType = record
+	name: string; { кличка }
+	breed: string; { порода }
+	rating: array[1..5] of integer; { пять оценок от 0 до 5 }
+end;
+
+program compare_sorting(stdin, stdout);
+begin
+end.
+
