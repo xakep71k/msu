11eb218ab81fe2e6cb5a946bce07b29207e6c4e0
Alexander <alexander.bubnov@gmail.com>
2020-10-23 10:26:37 +0300

    .

--- cpp/task1.3/sstack.cpp
+++ cpp/task1.3/sstack.cpp
@@ -1,18 +1,56 @@
 #include "sstack.h"
+#include <algorithm>
+#include <stdexcept>
+
+class RuntimeError : public std::runtime_error {
+    public:
+};
 
 class SStackImpl {
     public:
+        int index;
         int length;
+        const char** storage;
+
+        SStackImpl():length(0), storage(nullptr){
+            realloc(1);
+        }
+
+        SStackImpl(const SStackImpl &impl) {
+            realloc(impl.length);
+            std::copy(storage, storage + impl.index, impl.storage);
+            index = impl.index;
+        }
+
+        SStackImpl &operator=(const SStackImpl &impl) {
+            realloc(impl.length);
+            std::copy(storage, storage + impl.index, impl.storage);
+            index = impl.index;
+
+            return *this;
+        }
+
+        void realloc(int newlen) {
+            const char **newstorage = new const char*[newlen];
+            std::copy(newstorage, newstorage + index, newstorage);
+
+            delete []storage;
+            storage = newstorage;
+            length = newlen;
+        }
 };
 
 
-SStack::SStack(const char*):m_impl(new SStackImpl) {
+SStack::SStack(const char*):
+    m_impl(new SStackImpl) {
 }
 
-SStack::SStack(const SStack &stack):m_impl(new SStackImpl(*m_impl)) {
+SStack::SStack(const SStack &stack):
+    m_impl(new SStackImpl(*m_impl)) {
 }
 
 SStack::~SStack(){
+    delete []m_impl->storage;
 }
 
 SStack &SStack::operator=(const SStack &stack) {
