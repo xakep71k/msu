08dcd549a6d110fea16dff2663d687f0260fe4c6
Alexander Bubnov <alexander.bubnov@gmail.com>
2021-03-20 12:10:15 +0300

    .

--- mylang/scanner.h
+++ mylang/scanner.h
@@ -23,20 +23,26 @@ class Scanner
         }
         return 0;
     }
-    void gc() {
+    void gc()
+    {
         c = fgetc(fp);
-        if(EOF == c) {
-            if(feof(fp)) {
-                throw std::runtime_error("unexpected EOF");
-            }
+        if (ferror(fp))
+        {
             throw std::runtime_error(strerror(errno));
         }
     }
+    bool eof()
+    {
+        return std::feof(fp);
+    }
+
 public:
     static char **TW, **TD;
-    Scanner(const std::string &program) {
+    Scanner(const std::string &program)
+    {
         fp = fopen(program.c_str(), "r");
-        if(!fp) {
+        if (!fp)
+        {
             const std::string err_msg = program + std::string(": ") + strerror(errno);
             throw std::runtime_error(err_msg.c_str());
         }
