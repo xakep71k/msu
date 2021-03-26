c2b99513b89e41753e0e82f0c3722b881d6640b9
Alexander Bubnov <alexander.bubnov@gmail.com>
2021-03-27 00:04:10 +0300

    .

--- mylang/lex.h
+++ mylang/lex.h
@@ -27,6 +27,7 @@ enum type_of_lex
     LEX_WHILE,
     LEX_WRITE,
     LEX_WRITELN,
+    LEX_CASE,
     LEX_FIN,
     LEX_SEMICOLON,
     LEX_COMMA,
--- mylang/scanner.cpp
+++ mylang/scanner.cpp
@@ -24,6 +24,7 @@ const char *Scanner::TW[] = {
     "while",
     "write",
     "writeln",
+    "case",
     NULL};
 
 const char *Scanner::TD[] = {
