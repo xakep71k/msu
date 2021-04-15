f45cb2b7bb6ec10994bdd03606d83b0bdf0d22a5
Alexander Bubnov <alexander.bubnov@gmail.com>
2021-04-15 18:01:56 +0300

    .

--- mylang/tid.h
+++ mylang/tid.h
@@ -14,11 +14,30 @@ class TIDType
 public:
     TIDType() : current_func_("main") {}
 
-    const std::string &get_current_func() const { return current_func_; }
-    void set_current_func(const std::string &func) { current_func_ = func; }
-    Ident &operator[](int i) { return space_[current_func_][i]; }
-    void push_back(const Ident &ident) { space_[current_func_].push_back(ident); }
-    operator std::vector<Ident>&(){ return space_[current_func_]; }
+    const std::string &get_current_func() const
+    {
+        return current_func_;
+    }
+
+    void set_current_func(const std::string &func)
+    {
+        current_func_ = func;
+    }
+
+    Ident &operator[](int i)
+    {
+        return space_[current_func_][i];
+    }
+
+    void push_back(const Ident &ident)
+    {
+        space_[current_func_].push_back(ident);
+    }
+
+    operator std::vector<Ident> &()
+    {
+        return space_[current_func_];
+    }
 };
 
 extern TIDType TID;
