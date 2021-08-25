067f0a69846d035c3d85ef6ba44e40c49dcfea72
Alexander <26022428+xakep71k@users.noreply.github.com>
2021-08-25 21:35:11 +0300

    .

--- mylang/c++/interpretator.h
+++ mylang/c++/interpretator.h
@@ -3,6 +3,7 @@
 
 #include "parser.h"
 #include "executer.h"
+#include <chrono>
 
 class Interpretator {
     Parser   pars;
@@ -13,8 +14,16 @@ public:
 };
  
 void Interpretator::interpretation () {
+    std::chrono::steady_clock::time_point beginA = std::chrono::steady_clock::now();
     pars.analyze ();
+    std::chrono::steady_clock::time_point endA = std::chrono::steady_clock::now();
+
+    std::chrono::steady_clock::time_point beginE = std::chrono::steady_clock::now();
     E.execute ( pars.poliz );
+    std::chrono::steady_clock::time_point endE = std::chrono::steady_clock::now();
+
+    std::cout << "Analyzed: time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(endA - beginA).count() << "[µs]" << std::endl;
+    std::cout << "Executed: time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(endE - beginE).count() << "[µs]" << std::endl;
 }
 
 #endif
--- mylang/go/internal/app/mylang/interpreteter.go
+++ mylang/go/internal/app/mylang/interpreteter.go
@@ -1,7 +1,19 @@
 package mylang
 
+import (
+	"fmt"
+	"time"
+)
+
 func RunInterprepeter(sourceFile string) {
+	startA := time.Now()
 	parser := MakeParser(sourceFile)
 	poliz := parser.analyze()
+	durationA := time.Since(startA)
+
+	startE := time.Now()
 	executePolize(poliz)
+	durationE := time.Since(startE)
+	fmt.Printf("Analyzed: time difference = %v\n", durationA.Microseconds())
+	fmt.Printf("Executed: time difference = %v\n", durationE.Microseconds())
 }
