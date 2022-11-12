f4b2fab0d09b441c7d1f80077570142f3bcf51f6
Alexander Bubnov <alexander.bubnov@gmail.com>
2022-11-12 15:56:31 +0300

    .

--- unix/network/server.c
+++ unix/network/server.c
@@ -4,12 +4,14 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <errno.h>
 
-#define ECHO_PORT (2002)
-#define MAX_LINE (1024)
-
-void *thread_processing(void *);
+#define ECHO_PORT 2002
+#define MAX_LINE 1024
+#define MAX_CLIENTS 200
 
 typedef struct
 {
@@ -18,10 +20,15 @@ typedef struct
     size_t cap;
 } Buffer;
 
+int read_line(int fd, Buffer *buffer);
+void nulled_pollfd(struct pollfd *fd);
+
 int main(int argc, char *argv[])
 {
     int sockfd, newsockfd, clilen;
     struct sockaddr_in serv_addr, cli_addr;
+    struct pollfd fds[MAX_CLIENTS] = {0};
+    int nfds = 1;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -44,6 +51,24 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    /*************************************************************/
+    /* Set socket to be nonblocking. All of the sockets for      */
+    /* the incoming connections will also be nonblocking since   */
+    /* they will inherit that state from the listening socket.   */
+    /*************************************************************/
+    if (ioctl(sockfd, FIONBIO, (char *)&enable))
+    {
+        perror("setsockopt() failed");
+        close(sockfd);
+        exit(1);
+    }
+
+    memset(fds, 0, sizeof(fds));
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        fds[i].fd = -1;
+    }
+
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         perror("ERROR on binding");
@@ -56,26 +81,123 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    pthread_t thread_id;
+    fds[0].fd = sockfd;
+    fds[0].events = POLLIN;
 
-    clilen = sizeof(cli_addr);
+    Buffer buffer = {0};
+    int correct_nfds = 0;
 
-    while (1)
+    for (;;)
     {
-        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-        puts("client connected");
-
-        if (newsockfd < 0)
+        if (poll(fds, nfds, -1) < 0)
         {
-            perror("ERROR on accept");
-            continue;
+            perror("ERROR poll failed");
         }
 
-        if (pthread_create(&thread_id, NULL, thread_processing, (void *)&newsockfd) < 0)
+        for (int i = 0; i < nfds; i++)
         {
-            perror("could not create thread");
-            return 1;
+            if (fds[i].revents == 0)
+            {
+                continue;
+            }
+
+            if (fds[i].revents != POLLIN)
+            {
+                fprintf(stderr, " Error! revents = %d\n", fds[i].revents);
+                exit(1);
+            }
+
+            if (fds[i].fd == sockfd)
+            {
+                puts("listen socket");
+                int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+                if (newsockfd < 0)
+                {
+                    perror("ERROR accept()");
+                    continue;
+                }
+
+                int inserted = 0;
+                for (int j = 1; j < MAX_CLIENTS; j++)
+                {
+                    if (fds[j].fd == -1)
+                    {
+                        fds[j].fd = newsockfd;
+                        fds[j].events = POLLIN;
+                        inserted = j;
+                        break;
+                    }
+                }
+
+                if (!inserted)
+                {
+                    fprintf(stderr, "client queue is full, client rejected\n");
+                    close(newsockfd);
+                }
+                else
+                {
+                    if (inserted >= nfds)
+                    {
+                        nfds++;
+                    }
+                    puts("LOG: client connected");
+                }
+                break;
+            }
+
+            buffer.len = 0;
+            int rc = 0;
+            if ((rc = read_line(fds[i].fd, &buffer)) <= 0)
+            {
+                printf("LOG: rc = %d\n", rc);
+                if (rc < 0)
+                {
+                    perror("ERROR read_line()");
+                }
+                nulled_pollfd(&fds[i]);
+                puts("LOG: client exited1");
+                break;
+            }
+
+            if (strcmp(buffer.data, "quit\n") == 0)
+            {
+                nulled_pollfd(&fds[i]);
+                puts("LOG: client exited2");
+                break;
+            }
+
+            printf("%s", buffer.data);
+            for (int j = 1; j < nfds; j++)
+            {
+                if (fds[j].fd == -1)
+                {
+                    continue;
+                }
+
+                if (write(fds[j].fd, buffer.data, buffer.len) < 0)
+                {
+                    perror("cannot write data to client");
+                    nulled_pollfd(&fds[j]);
+                    puts("LOG: client exited3");
+                }
+            }
         }
+
+        int current_size = nfds;
+        for (int i = 0; i < nfds; i++)
+        {
+            if (fds[i].fd == -1)
+            {
+                continue;
+            }
+
+            current_size = i;
+        }
+
+        current_size++;
+        printf("LOG: current size was %d, become %d\n", nfds, current_size);
+        nfds = current_size;
     }
 }
 
@@ -105,40 +227,46 @@ void append_char(Buffer *buffer, char ch)
     buffer->data[buffer->len] = 0;
 }
 
-int read_line(FILE *file, Buffer *buffer)
+int read_line(int fd, Buffer *buffer)
 {
-    int ch;
+    char ch;
     buffer->len = 0;
 
-    while ((ch = getc(file)) != EOF)
+    for (;;)
     {
+        char ch;
+        ssize_t size = read(fd, &ch, sizeof(ch));
+        if (size <= 0)
+        {
+            return size;
+        }
+
         append_char(buffer, ch);
         if (ch == '\n')
         {
-            break;
+            return buffer->len;
         }
     }
 
     return ch;
 }
 
-void processing_client(FILE *fp)
+void processing_client(int sock)
 {
     Buffer buffer = {0};
-    while (read_line(fp, &buffer) != EOF)
+
+    while (read_line(sock, &buffer) != EOF)
     {
         if (strcmp(buffer.data, "quit\n") == 0)
         {
             break;
         }
 
-        if (fwrite(buffer.data, sizeof(buffer.data), buffer.len, fp) != buffer.len)
+        if (write(sock, buffer.data, buffer.len) != buffer.len)
         {
             perror("ERROR on write to client");
             break;
         }
-
-        fflush(fp);
 
         buffer.len = 0;
     }
@@ -151,18 +279,9 @@ void processing_client(FILE *fp)
     return;
 }
 
-void *thread_processing(void *arg)
+void nulled_pollfd(struct pollfd *fd)
 {
-    int sock = *(int *)arg;
-    FILE *fp = fdopen(sock, "r+");
-    if (fp != NULL)
-    {
-        processing_client(fp);
-        fclose(fp);
-    }
-    else
-    {
-        perror("fdopen failed");
-    }
-    puts("client exited");
+    fd->events = 0;
+    fd->fd = -1;
+    fd->revents = 0;
 }
